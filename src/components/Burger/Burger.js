e99bb4e702dc8176e23235b769946d5e82e810f8
Ashish Prabhune <ashuprabhune@gmail.com>
2020-09-14 22:51:18 -0400

    Added CSS for displaying burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Burger.module.css'
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+  .map(igKey =>{
+      return [...Array(props.ingredients[igKey])].map((_,index) =>{
+        return <BurgerIngredients key={igKey + index} type={igKey} />
+      });
+  });
+  return(
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredients type="bread-bottom" />
+
+    </div>
+  );
+}
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
-import Aux from '../../hoc/Auxilliary'
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+  state = {
+    ingredients : {
+      salad : 1,
+      meat : 2,
+      cheese :2,
+      bacon : 1
+    }
+  }
   render(){
     return(
       <Aux>
-        <div> Burger </div>
+        <Burger ingredients = {this.state.ingredients}/>
         <div> BurgerBuilder </div>
       </Aux>
     );
